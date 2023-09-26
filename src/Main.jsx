6c9637ffb62a392b96fd70f9a3b65262ecb81066
Khaleed <Khaleedopeloyerudev@gmail.com>
2023-09-26 08:20:20 -0400

    added style

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Fragment>
-    <App/>
+    <App />
   </React.Fragment>
-)
+);
